7964895cb971e4d02c44b73306fc70d7bbadbc8d
loganatkinson <logan.atkinson@gmail.com>
2021-06-18 17:04:02 -0400

    added blank templates and started to add functions in views

--- albums/views.py
+++ albums/views.py
@@ -1,3 +1,62 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Album
+from .forms import AlbumForm
 # Create your views here.
+
+def list_album(request):
+    albums = Album.objects.all()
+    return render(request, "albums/templates/albums/list_albums.html",
+                  {"albums": albums})
+
+#------------------------------------paused---------
+def add_contact(request):
+    if request.method == 'GET':
+        form = ContactForm()
+    else:
+        form = ContactForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(to='list_contacts')
+
+    return render(request, "contacts/add_contact.html", {"form": form})
+
+
+def edit_contact(request, pk):
+    contact = get_object_or_404(Contact, pk=pk)
+    if request.method == 'GET':
+        form = ContactForm(instance=contact)
+    else:
+        form = ContactForm(data=request.POST, instance=contact)
+        if form.is_valid():
+            form.save()
+            return redirect(to='list_contacts')
+
+    return render(request, "contacts/edit_contact.html", {
+        "form": form,
+        "contact": contact
+    })
+
+
+def delete_contact(request, pk):
+    contact = get_object_or_404(Contact, pk=pk)
+    if request.method == 'POST':
+        contact.delete()
+        return redirect(to='list_contacts')
+
+    return render(request, "contacts/delete_contact.html",
+        {"contact": contact})
+
+def add_note(request, pk);
+    note = get_object_or_404(Contact, pk=pk)
+    if request.method == 'GET':
+        form = NoteForm()
+    else:
+        form = NoteForm(data=request.POST)
+
+        if form.is_valid():
+            note = form.save(commit=False)
+            note.contact = contact
+            note.save()
+            return redirect(to='list_contacts')
+
+    return render(request, "contacts/add_note.html", {"form": form, "contact": contact})
