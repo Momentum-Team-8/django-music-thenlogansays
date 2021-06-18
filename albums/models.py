c211ff298fe8cb7683977b357912157762704963
loganatkinson <logan.atkinson@gmail.com>
2021-06-18 14:45:40 -0400

    added album model created superuserfor admin

---
+++ albums/models.py
@@ -0,0 +1,10 @@
+
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+class Album(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.CharField(max_length=500)
+    created_at = models.DateTimeField(default=timezone.now)
