3f52cfc5f8e356d8002b79ac909263ab851ec19e
loganatkinson <logan.atkinson@gmail.com>
2021-06-18 17:20:52 -0400

    def attribute new_album to views.py

--- albums/views.py
+++ albums/views.py
@@ -6,5 +6,17 @@ from .forms import AlbumForm
 def list_album(request):
     albums = Album.objects.all()
     return render(request, "albums/templates/albums/list_albums.html",
-                  {"albums": albums})
+                {"albums": albums})
+
+def new_album(request):
+    
+    if request.method == 'GET':
+        form = AlbumForm()
+    else:
+        form = AlbumForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(to='list_albums')
+
+    return render(request, "albums/templates/albums/add_album.html", {"form": form})
 
