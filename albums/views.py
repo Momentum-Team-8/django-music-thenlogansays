45c3447f94612a3cc5985120f6cccf5534fba8be
loganatkinson <logan.atkinson@gmail.com>
2021-06-19 19:00:05 -0400

    added functions for edit_album and delete_album attributes in views.py

--- albums/views.py
+++ albums/views.py
@@ -20,3 +20,27 @@ def new_album(request):
 
     return render(request, "albums/templates/albums/add_album.html", {"form": form})
 
+def edit_album(request, pk):
+    album = get_object_or_404(Album, pk=pk)
+    if request.method == 'GET':
+        form = AlbumForm(instance=album)
+    else:
+        form = AlbumForm(data=request.POST, instance=album)
+        if form.is_valid():
+            form.save()
+            return redirect(to='list_albums')
+
+    return render(request, "albums/templates/albums/edit_album.html", {
+        "form"= form,
+        "album"= album,
+    })
+
+    def delete_album(request, pk):
+        album= get_object_or_404(Album, pk=pk)
+        if request.method == 'POST':
+            album.delete()
+            return redirect(to='list_albums')
+
+    return render(request, "albums/templates/albums/delete_album.html", {
+        "album": album
+    })
